352fe929291ae3cdff15290581b2d141d25811c6
NaveenWebDev <ns222280@gmail.com>
2023-08-01 13:43:32 +0530

    user Configration set

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import logo from '../assets/logo-removebg-preview.png'
-import { Link } from 'react-router-dom'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 import { initializeApp } from 'firebase/app'
 import firebaseConfig from './FirebaseCon'
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
@@ -10,17 +10,23 @@ const SignInForm = () => {
     const app = initializeApp(firebaseConfig);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const Navigate = useNavigate();
+    const [user, setUser] = useState(false)
 
     const auth = getAuth();
 
     const signin = (e) =>{
+        e.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
         .then(auth =>{
             if(auth){
-                alert("success")
+                alert("success");
+                Navigate('/banner');
             }
         })
-        .catch(error => alert(error.message));
+        .catch(error => {
+            setUser(true)
+        });
 
     }
 
@@ -35,11 +41,14 @@ const SignInForm = () => {
                     <form className='bg-[#0000008c] text-white flex flex-col gap-4 p-10 rounded-md'>
                         <h2 className='font-bold text-3xl'>Sign In</h2>
 
-                        <input type="text" value={email} className='bg-[#323233] text-slate-400 px-8 py-3 rounded-md' name='email' placeholder='Email or Phone Number' onChange={(e)=>setEmail(e.target.value)} />
+                        <input type="text" value={email} className='bg-[#323233] text-slate-400 px-8 py-3 rounded-md' name='email' placeholder='Email or Phone Number' required onChange={(e)=>setEmail(e.target.value)} />
 
-                        <input type="password" value={password} className='bg-[#323233] text-slate-400 px-8 py-3 rounded-md' name="password" placeholder='Password'  onChange={(e)=>setPassword(e.target.value)} />
+                        <input type="password" value={password} className='bg-[#323233] text-slate-400 px-8 py-3 rounded-md' name="password" placeholder='Password' required  onChange={(e)=>setPassword(e.target.value)} />
                         
-                        <Link to="/banner" onClick={signin} className='bg-[#E40915] rounded-md px-8 py-3 text-center'>Sign In</Link>
+                        <button onClick={signin} className='bg-[#E40915] rounded-md px-8 py-3 text-center'>Sign In</button>
+
+                        { user?<p className='text-red-700'>User not found Please signUP Now</p> : null}
+
                         <div className='flex justify-between'>
                         <div>
                             <input type="checkbox" name="check" /> <span>Remember me</span>
