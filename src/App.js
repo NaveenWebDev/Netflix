7d3bb3cea1468252620141823c6532c99c066a57
NaveenWebDev <ns222280@gmail.com>
2023-08-02 11:19:10 +0530

    Login & SignUP Authenticaton completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import './index.css';
-import Main from './components/Main';
-import SignInForm from './components/SignInForm';
-import Banner from './components/Banner';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./index.css";
+import Main from "./components/Main";
+import SignInForm from "./components/SignInForm";
+import Banner from "./components/Banner";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<Main></Main>}/>
-          <Route path='/SignIn' element={<SignInForm></SignInForm>}/>
-          <Route path='/banner' element={<Banner></Banner>}/>
+          <Route path="/" element={<Main></Main>} />
+          <Route
+            path="/SignIn"
+            element={<SignInForm page={true}></SignInForm>}
+          />
+          <Route
+            path="/SignUp"
+            element={<SignInForm page={false}></SignInForm>}
+          />
+          <Route path="/banner" element={<Banner></Banner>} />
         </Routes>
       </Router>
-
     </div>
   );
 }
--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,67 +1,115 @@
-import React, { useState } from 'react'
-import logo from '../assets/logo-removebg-preview.png'
-import { Link, Navigate, useNavigate } from 'react-router-dom'
-import { initializeApp } from 'firebase/app'
-import firebaseConfig from './FirebaseCon'
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import logo from "../assets/logo-removebg-preview.png";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./FirebaseCon";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 
-const SignInForm = () => {
+const SignInForm = ({ page }) => {
+  const app = initializeApp(firebaseConfig);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const Navigate = useNavigate();
+  const [user, setUser] = useState(false);
 
-    const app = initializeApp(firebaseConfig);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const Navigate = useNavigate();
-    const [user, setUser] = useState(false)
+  const auth = getAuth();
 
-    const auth = getAuth();
+  const signin = (e) => {
+    e.preventDefault();
+    if(page){
 
-    const signin = (e) =>{
-        e.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
-        .then(auth =>{
-            if(auth){
-                alert("success");
-                Navigate('/banner');
-            }
+      .then((auth) => {
+          if (auth) {
+          alert("Login success");
+          Navigate("/banner");
+        }
+    })
+    .catch((error) => {
+        setUser(true);
+    });
+    }else{
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(auth => {
+            alert("signUp Successful");
+            setEmail("")
+            setPassword("")
         })
         .catch(error => {
-            setUser(true)
-        });
-
+            alert(error)
+            setEmail("")
+            setPassword("")
+        })
     }
+  };
 
   return (
     <>
-        <div className='formHeader h-screen relative'>
+      <div className="formHeader h-screen relative">
         <div className="absolute w-full bg-[#0000007c] h-full z-10"></div>
-            <div className="nav z-20 absolute w-full">
-                <img src={logo} alt="logo" width="250" className='p-5'/>
-            </div>
-                <div className="form grid place-items-center z-20 absolute w-full h-full">
-                    <form className='bg-[#0000008c] text-white flex flex-col gap-4 p-10 rounded-md'>
-                        <h2 className='font-bold text-3xl'>Sign In</h2>
-
-                        <input type="text" value={email} className='bg-[#323233] text-slate-400 px-8 py-3 rounded-md' name='email' placeholder='Email or Phone Number' required onChange={(e)=>setEmail(e.target.value)} />
-
-                        <input type="password" value={password} className='bg-[#323233] text-slate-400 px-8 py-3 rounded-md' name="password" placeholder='Password' required  onChange={(e)=>setPassword(e.target.value)} />
-                        
-                        <button onClick={signin} className='bg-[#E40915] rounded-md px-8 py-3 text-center'>Sign In</button>
-
-                        { user?<p className='text-red-700'>User not found Please signUP Now</p> : null}
-
-                        <div className='flex justify-between'>
-                        <div>
-                            <input type="checkbox" name="check" /> <span>Remember me</span>
-                        </div>
-                        <span>need help?</span>
-                        </div>
-                        <p>New to Netflix? <Link to="/">Sign Up </Link></p>
-
-                    </form>
-                </div>
+        <div className="nav z-20 absolute w-full">
+          <img src={logo} alt="logo" width="250" className="p-5" />
         </div>
-    </>
-  )
-}
+        <div className="form grid place-items-center z-20 absolute w-full h-full">
+          <form className="bg-[#0000008c] text-white flex flex-col gap-4 p-10 rounded-md">
+            <h2 className="font-bold text-3xl">
+              {page ? "Sign In" : "Sign Up"}
+            </h2>
 
-export default SignInForm
+            <input
+              type="text"
+              value={email}
+              className="bg-[#323233] text-slate-400 px-8 py-3 rounded-md"
+              name="email"
+              placeholder="Email or Phone Number"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input
+              type="password"
+              value={password}
+              className="bg-[#323233] text-slate-400 px-8 py-3 rounded-md"
+              name="password"
+              placeholder="Password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button
+              onClick={signin}
+              className="bg-[#E40915] rounded-md px-8 py-3 text-center"
+            >
+              {page ? "Sign In" : "Sign Up"}
+            </button>
+
+            {user ? (
+              <p className="text-red-700">
+                User not found Please go to signUp Now
+              </p>
+            ) : null}
+
+            { page ?
+            <div className="flex justify-between">
+
+              <div>
+                <input type="checkbox" name="check" /> <span>Remember me</span>
+              </div>
+              <span>need help?</span>
+            </div> : null
+            }
+            <p>
+              New to Netflix?{" "}
+              <Link to={page ? "/SignUp" : "/SignIn"}>
+                {" "} 
+                {page ? "Sign Up" : "Sign In"}{" "}
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignInForm;
