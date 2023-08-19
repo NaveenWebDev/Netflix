9f7006f0a6bcf853fe7c3383504ea16acd3e3dbd
NaveenWebDev <ns222280@gmail.com>
2023-08-19 15:34:36 +0530

    api data added on banner page

--- src/components/List.js
+++ src/components/List.js
@@ -19,32 +19,52 @@ const List = () => {
         .then((res) => res.json())
         .then((res) => {
             const categories = res.genres;
-            // if (Array.isArray(categories) && categories.length){
-                //     categories.forEach(category => {
-                    //         fetchAndbuildMovieSection(apiPaths.fetchMoviesList(category.id));
-          //     });
-          // }
+            if (Array.isArray(categories) && categories.length){
+                    categories.forEach(category => {
+                            fetchAndbuildMovieSection(apiPaths.fetchMoviesList(category.id));
+              });
+          }
           setCato(categories);
         })
         .catch((err) => console.error(err));
   }, []);
 
-  // function fetchAndbuildMovieSection(categories){
-  //     fetch(categories)
-  //         .then(res=>res.json())
-  //         .then(data=> {
-  //             console.log(data.results)
-  //         })
-  // }
+  function fetchAndbuildMovieSection(categories){
+      fetch(categories)
+          .then(res=>res.json())
+          .then(data=> {
+              console.log(data.results[0].backdrop_path)
+          })
+  }
+  console.log(cato)
 
   return (
     <>
       {
-        cato.map((value) => {
-        <div className="border border-red-700 h-[250px]">
-          <h2 className="text-2xl font-bold p-1">{value}</h2>
-          <div className="img border border-blue-500"></div>
-        </div>;
+        cato.map((value) => { 
+          return(
+        <div className="border border-red-700">
+          <h2 className="text-2xl font-bold">{value.name}</h2>
+          <div className="border border-blue-500 overflow-x-scroll">
+
+          <div className="img w-[200px] bg-slate-500 flex m-2 gap-3">
+            <img 
+              src="https://koimoi.com/wp-content/new-galleries/2015/12/airlift-movie-poster-4.jpg" 
+              alt="moviePoster" 
+              height="100%"
+              width="100%"
+            />
+            <img 
+              src="https://koimoi.com/wp-content/new-galleries/2015/12/airlift-movie-poster-4.jpg" 
+              alt="moviePoster" 
+              height="100%"
+              width="100%"
+            />
+          </div>
+
+          </div>
+        </div>
+          )
       })}
     </>
   );
